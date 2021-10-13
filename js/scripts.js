8b00d78b85c3da0eb01b2ad3ed0a1d4c25e1a818
Antonio Andraues Jr <antoniojaj@al.insper.edu.br>
2021-10-13 08:00:15 -0300

    Updates

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,8 @@
+/*!
+* Start Bootstrap - Grayscale v7.0.3 (https://antonioandraues.github.io/portfolio)
+* Copyright 2013-2021 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-grayscale/blob/master/LICENSE)
+*/
 //
 // Scripts
 // 
